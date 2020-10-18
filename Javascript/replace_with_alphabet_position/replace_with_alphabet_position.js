bfefe951df8113b4ae7fd4aa94df6ccc5e857319
GoodGuyGregory <greg.witt625@gmail.com>
2020-10-18 17:24:35 -0400

    adds gitignore and initial js solution files

---
+++ Javascript/replace_with_alphabet_position/replace_with_alphabet_position.js
@@ -0,0 +1,12 @@
+// Problem: https://www.codewars.com/kata/546f922b54af40e1e90001da/javascript
+
+// Solution:
+function alphabetPosition(alphaString){
+
+}
+
+
+
+// Example:
+
+alphabetPosition("The sunset sets at twelve o' clock.");
