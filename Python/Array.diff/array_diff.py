319bd354893321816876e76f61bc42c84ecdad87
GoodGuyGregory <greg.witt625@gmail.com>
2020-10-19 11:44:30 -0400

    adds Python Kata

---
+++ Python/Array.diff/array_diff.py
@@ -0,0 +1,15 @@
+# Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/python
+
+
+def array_diff(a, b):
+
+
+main():
+
+    #   Examples:
+
+    # It should remove all values from list a, which are present in list b.
+    # array_diff([1,2],[1]) == [2]
+
+    # If a value is present in b, all of its occurrences must be removed from the other:
+    # array_diff([1, 2, 2, 2, 3], [2]) == [1, 3]
