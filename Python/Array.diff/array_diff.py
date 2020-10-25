d3c38cc3945fa55f77917c497906c165eceb205e
GoodGuyGregory <greg.witt625@gmail.com>
2020-10-25 11:41:47 -0400

    adds array_diff problem to python solutions

--- Python/Array.diff/array_diff.py
+++ Python/Array.diff/array_diff.py
@@ -2,14 +2,30 @@
 
 
 def array_diff(a, b):
+    c = []
+    for i in range(len(a)):
+        if a[i] not in b:
+            c.append(a[i])
+    return c
 
 
-main():
+def main():
 
     #   Examples:
 
     # It should remove all values from list a, which are present in list b.
-    # array_diff([1,2],[1]) == [2]
+    # array_diff([1, 2], [1]) == [2]
+    print(array_diff([1, 2], [1]))
 
     # If a value is present in b, all of its occurrences must be removed from the other:
     # array_diff([1, 2, 2, 2, 3], [2]) == [1, 3]
+
+    print(array_diff([1, 2, 2, 2, 3], [2]))
+
+    # If a value is present in b, all of its occurrences must be removed from the other:
+    #array_diff([1,2,2], [1]) == [2, 2]
+
+    print(array_diff([1, 2, 2], [1]))
+
+
+main()
