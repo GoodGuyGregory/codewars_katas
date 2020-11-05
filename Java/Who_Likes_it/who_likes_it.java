74c210c82152f82c0957cc2926ff4b76bbd62c91
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-04 20:31:49 -0500

    adds Java notes and initial commit for kata

--- Java/Who_Likes_it/who_likes_it.java
+++ Java/Who_Likes_it/who_likes_it.java
@@ -4,10 +4,16 @@
 package Java.Who_Likes_it;
 
 public class who_likes_it {
-    class Solution {
-        public static String whoLikesIt(String... names) {
-            // Do your magic here
-            return "";
+
+    public static String whoLikesIt(String... names) {
+        if (names.length == 0) {
+            return "no one likes this";
+        } else {
+            String likesCollection = "";
+            for (int i = 0; i < names.length; i++) {
+                likesCollection += names[i] + " likes this";
+            }
+            return likesCollection;
         }
     }
 
@@ -15,13 +21,27 @@ public class who_likes_it {
 
         // TESTS:
         // likes {} // must be "no one likes this"
+        String[] likes = {};
+
+        who_likes_it likesTest = new who_likes_it();
+
+        System.out.println(likesTest.whoLikesIt(likes));
+
         // likes {"Peter"} // must be "Peter likes this"
+
+        String[] likeswithOne = { "Peter" };
+
+        System.out.println(likesTest.whoLikesIt(likeswithOne));
+
         // likes {"Jacob", "Alex"} // must be "Jacob and Alex like this"
+
+        String[] likeswithTwo = { "Jacob", "Alex" };
+
+        System.out.println(likesTest.whoLikesIt(likeswithTwo));
+
         // likes {"Max", "John", "Mark"} // must be "Max, John and Mark like this"
         // likes {"Alex", "Jacob", "Mark", "Max"} // must be "Alex, Jacob and 2 others
         // like this"
 
-        // RESOURCES STATIC METHODS:
-        // https://www.tutorialspoint.com/Java-static-method
     }
 }
