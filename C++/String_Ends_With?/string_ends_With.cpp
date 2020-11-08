d9aa43758eeef69cba7afad49969c108b549630a
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-08 00:09:04 -0500

    adds string ends with in cpp

---
+++ C++/String_Ends_With?/string_ends_With.cpp
@@ -0,0 +1,14 @@
+// Kata: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/cpp
+
+bool solution(std::string const &str, std::string const &ending)
+{
+    return true;
+}
+
+int main()
+{
+    // Examples:
+    solution('abc', 'bc') // returns true
+
+        solution('abc', 'd') // returns false
+}
