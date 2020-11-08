bf372833b9a50350b3c896da44a13d2981f457be
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-08 15:14:31 -0500

    adds an attempt strategy for kata

--- C++/String_Ends_With?/string_ends_With.cpp
+++ C++/String_Ends_With?/string_ends_With.cpp
@@ -1,8 +1,32 @@
 // Kata: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/cpp
+#include <cstring>
 
 bool solution(std::string const &str, std::string const &ending)
 {
-    return true;
+    // if the string length is larger than a single char
+    if (strlen(ending) == 1)
+    {
+        if ()
+        {
+            // found letter in string
+            return true;
+        }
+        else
+            // not found
+            return false;
+    }
+    else
+    {
+
+        if (strstr(str, ending) != NULL))
+            {
+                return true;
+            }
+        else
+        {
+            return false;
+        }
+    }
 }
 
 int main()
