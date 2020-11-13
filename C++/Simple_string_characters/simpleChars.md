d0f906a184407c1ee505c8fa2d27b78ef8da8ab8
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-13 09:06:07 -0500

    adds notes on regex for Simple_String_chars kata

---
+++ C++/Simple_string_characters/simpleChars.md
@@ -0,0 +1,4 @@
+## Simple String Characters Reference Notes:
+
+[Regex in C++](https://www.softwaretestinghelp.com/regex-in-cpp/)
+
--- C++/Simple_string_characters/simple_string_chars.cpp
+++ C++/Simple_string_characters/simple_string_chars.cpp
@@ -1,4 +1,5 @@
 // Kata: https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/train/cpp
+
 using namespace std;
 
 std::vector<int> solve(std::string s)
