dc263ef5d05aa273800665d8aa9ef2a8743f1821
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-12 20:02:00 -0500

    adds C++ Example

--- C++/Simple_string_characters/simple_string_chars.cpp
+++ C++/Simple_string_characters/simple_string_chars.cpp
@@ -8,4 +8,7 @@ std::vector<int> solve(std::string s)
 
 int main()
 {
+    //  EXAMPLES:
+    solve("*'&ABCDabcde12345"); // = [ 4, 5, 5, 3 ]
+    // --the order is : uppercase letters, lowercase, numbers and special characters.
 }
