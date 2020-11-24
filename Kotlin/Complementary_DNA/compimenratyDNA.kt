9437abc56f4c3b143bfbb62de49ab8779603dbfd
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-24 12:35:23 -0500

    adds basic idea of solution

--- Kotlin/Complementary_DNA/compimenratyDNA.kt
+++ Kotlin/Complementary_DNA/compimenratyDNA.kt
@@ -1,11 +1,24 @@
 // Kata: https://www.codewars.com/kata/554e4a2f232cdd87d9000038/train/kotlin
 
-fun makeComplement(dna: String) : String {
-
+fun makeComplement(dna: String): String {
+    // iterate through sequence
+    for (proteinBase in dna) {
+        // Use When to sequence DNA
+        when (proteinBase) {
+            "A" -> "T"
+            "C" -> "G"
+            "G" -> "C"
+            "T" -> "A"
+        }
+        print(proteinBase)
+    }
+    return dna
 }
 
-
 fun main() {
+    // Examples Provided from Kata:
 
+    makeComplement("ATTGC") // returns "TAACG"
 
+    makeComplement("GTAT") // returns "CATA"
 }
