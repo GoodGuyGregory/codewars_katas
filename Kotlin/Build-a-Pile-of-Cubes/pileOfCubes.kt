52ad62848236d9c88df1cd594e7c05d28ad25e93
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-25 13:21:15 -0500

    initial commit for building cubes kata

---
+++ Kotlin/Build-a-Pile-of-Cubes/pileOfCubes.kt
@@ -0,0 +1,13 @@
+// Kata: https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/kotlin
+
+object ASum {
+
+    fun findNb(m: Long): Long {
+        // your code
+    }
+}
+
+// Create object for test methods:
+
+// findNb(1071225) --> 45
+// findNb(91716553919377) --> -1
