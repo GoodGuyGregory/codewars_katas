e988a1abdbb7ad9d8f3272b159dd87029e185e7f
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-25 13:30:02 -0500

    adds some ideas of solution to kata

--- Kotlin/Build-a-Pile-of-Cubes/pileOfCubes.kt
+++ Kotlin/Build-a-Pile-of-Cubes/pileOfCubes.kt
@@ -1,9 +1,22 @@
 // Kata: https://www.codewars.com/kata/5592e3bd57b64d00f3000047/train/kotlin
+import kotlin.math.*
 
 object ASum {
 
     fun findNb(m: Long): Long {
         // your code
+        var cubes: Long = 0
+        while (m !== 0) {
+            m = cbrt(m)
+            cubes += 1
+            findNb(m)
+        }
+        if 
+
+        return -1
+
+        else 
+            return cubes
     }
 }
 
