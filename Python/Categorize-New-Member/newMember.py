46573a91cef5db248a4b28477e1d526140cba70c
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-28 14:50:20 -0500

    adds complete new member kata

--- Python/Categorize-New-Member/newMember.py
+++ Python/Categorize-New-Member/newMember.py
@@ -2,13 +2,27 @@
 
 
 def open_or_senior(data):
-    return
+    memberRank = []
+    for i in range(len(data)):
+        # print(data[i][0])
+        if data[i][0] >= 55 and data[i][1] > 7:
+            memberRank.append("Senior")
+        else:
+            memberRank.append("Open")
+    return memberRank
+
+# Very Nice Solution
+
+
+def openOrSeniorDefinedKeys(data):
+    return ["Senior" if age >= 55 and handicap >= 8 else "Open" for (age, handicap) in data]
 
 
 def main():
     # Examples
     # Input: [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
-    # open_or_senior([[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]])
+    print(open_or_senior(
+        [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
 
     # Expected Output:
     # ["Open", "Open", "Senior", "Open", "Open", "Senior"]
