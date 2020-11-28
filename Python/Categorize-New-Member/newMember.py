4009b3754fe3ac941b0bd4bedfd07644f3495a7b
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-28 11:02:58 -0500

    initial commit for new member kata

---
+++ Python/Categorize-New-Member/newMember.py
@@ -0,0 +1,17 @@
+# Kata: https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/train/python
+
+
+def open_or_senior(data):
+    return
+
+
+def main():
+    # Examples
+    # Input: [[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]]
+    # open_or_senior([[18, 20],[45, 2],[61, 12],[37, 6],[21, 21],[78, 9]])
+
+    # Expected Output:
+    # ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+
+
+main()
