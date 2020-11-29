cda5dd1c9abf64d02f1621e9c85b85bf60c69898
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-29 10:55:49 -0500

    renames files and adds progress on Vasya Kata

---
+++ Java/Vasya_Clerk/Line.java
@@ -0,0 +1,92 @@
+//  Kata: https://www.codewars.com/kata/555615a77ebc7c2c8a0000b8/train/java
+
+public class Line {
+    public static String Tickets(int[] peopleInLine) {
+        // three spots for each bill and 10 slots for them.
+        int[][] cashDrawer = new int[3][5];
+        for (int payment : peopleInLine) {
+            System.out.println("The ticket is $25.00 ");
+            if (payment == 25) {
+                for (int i = 0; i < cashDrawer[0].length; i++) {
+                    if (cashDrawer[0][i] == 0) {
+                        cashDrawer[0][i] = 25;
+                        break;
+                    }
+                }
+                System.out.println("Thank You! Enjoy the show!");
+            }
+            // Recieve a 50
+            if (payment == 50) {
+                System.out.println("Change for $50.00");
+                // place in cashdrawer
+                for (int i = 0; i < cashDrawer[1].length; i++) {
+                    if (cashDrawer[1][i] == 0) {
+                        cashDrawer[1][i] = 50;
+                        break;
+                    }
+                }
+                // Give Change from the cashDrawer
+                for (int i = cashDrawer[0].length - 1; 0 < i; i--) {
+                    if (cashDrawer[0][i] == 25) {
+                        cashDrawer[0][i] = 0;
+                        break;
+                    } else if (cashDrawer[0][i] == 0 && i == 0) {
+                        return "NO";
+                    }
+                }
+                System.out.println("Thank You! Enjoy the show!");
+
+            }
+            // Recieve a 100
+            if (payment == 100) {
+                System.out.println("Change for $100.00");
+                // Place 100 in the drawer
+                for (int i = 0; i < cashDrawer[2].length; i++) {
+                    if (cashDrawer[2][i] == 0) {
+                        cashDrawer[2][i] = 100;
+                        break;
+                    }
+                }
+                // Give Change of 75
+                for (int i = cashDrawer[1].length - 1; 0 < i; i--) {
+                    if (cashDrawer[1][i] == 50) {
+                        cashDrawer[1][i] = 0;
+                        break;
+                    } else if (cashDrawer[1][i] == 0 && i == 0) {
+                        return "NO";
+                    }
+                }
+                for (int i = cashDrawer[0].length - 1; 0 < i; i--) {
+                    if (cashDrawer[0][i] == 25) {
+                        cashDrawer[0][i] = 0;
+                        break;
+                    } else if (cashDrawer[0][i] == 0 && i == 0) {
+                        return "NO";
+                    }
+                }
+                System.out.println("Thank You! Enjoy the show!");
+
+            }
+            // Display cashDrawer Contents
+            System.out.println("Cash Drawer: ");
+            System.out.println("=======================");
+            for (int[] c : cashDrawer) {
+                for (int i : c) {
+                    System.out.print(i + "\t");
+                }
+                System.out.println("\n");
+            }
+        }
+        return "YES";
+    }
+
+    public static void main(String[] args) {
+        // Examples:
+        Line examples = new Line();
+
+        System.out.println(examples.Tickets(new int[] { 25, 25, 50 }));// => YES
+
+        // => NO. Vasya will not have enough money to give change to 100 dollars
+        System.out.println(Line.Tickets(new int[] { 25, 100 }));
+    }
+}
---
+++ Java/Vasya_Clerk/notes.md
@@ -0,0 +1,3 @@
+[Two-Dimensional Array Refresher](https://www.java67.com/2014/10/how-to-create-and-initialize-two-dimensional-array-java-example.html)
+
+[ForEach Article in G4G](https://www.geeksforgeeks.org/for-each-loop-in-java/)
