bb3baf21a91de0918cd986a86f8868a4410fbe7e
GoodGuyGregory <greg.witt625@gmail.com>
2020-11-29 00:25:18 -0500

    inital commit for vasya

---
+++ Java/Vasya - Clerk/vasya_clerk.java
@@ -0,0 +1,11 @@
+//  Kata: https://www.codewars.com/kata/555615a77ebc7c2c8a0000b8/train/java
+
+public class Line {
+    public static String Tickets(int[] peopleInLine) {
+        // Your code is here...
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
