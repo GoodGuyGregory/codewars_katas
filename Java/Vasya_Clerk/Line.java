9e427e29ed1431c1e04b3c2f83e27d1b6def2797
GoodGuyGregory <greg.witt625@gmail.com>
2020-12-01 15:21:52 -0500

    adds solution to line kata

--- Java/Vasya_Clerk/Line.java
+++ Java/Vasya_Clerk/Line.java
@@ -3,7 +3,7 @@
 public class Line {
     public static String Tickets(int[] peopleInLine) {
         // three spots for each bill and 10 slots for them.
-        int[][] cashDrawer = new int[3][5];
+        int[][] cashDrawer = new int[3][10];
         for (int payment : peopleInLine) {
             System.out.println("The ticket is $25.00 ");
             if (payment == 25) {
@@ -72,21 +72,24 @@ public class Line {
 
                 }
 
-                // Give Change of 75
-                for (int i = cashDrawer[1].length - 1; 0 < i; i--) {
-                    if (cashDrawer[1][i] == 50) {
-                        cashDrawer[1][i] = 0;
-                        break;
-                    } else if (cashDrawer[1][i] == 0 && i == 0) {
-                        return "NO";
+                // Give Change of 75 with 50s and 25s
+                if (cashDrawer[1][0] != 0 && cashDrawer[0][1] != 0) {
+
+                    for (int i = cashDrawer[1].length - 1; 0 <= i; i--) {
+                        if (cashDrawer[1][i] == 50) {
+                            cashDrawer[1][i] = 0;
+                            break;
+                        } else if (cashDrawer[1][i] == 0 && i == 0) {
+                            return "NO";
+                        }
                     }
-                }
-                for (int i = cashDrawer[0].length - 1; 0 < i; i--) {
-                    if (cashDrawer[0][i] == 25) {
-                        cashDrawer[0][i] = 0;
-                        break;
-                    } else if (cashDrawer[0][i] == 0 && i == 0) {
-                        return "NO";
+                    for (int i = cashDrawer[0].length - 1; 0 <= i; i--) {
+                        if (cashDrawer[0][i] == 25) {
+                            cashDrawer[0][i] = 0;
+                            break;
+                        } else if (cashDrawer[0][i] == 0 && i == 0) {
+                            return "NO";
+                        }
                     }
                 }
                 System.out.println("Change for $100.00");
@@ -128,9 +131,17 @@ public class Line {
         // System.out.println(examples.Tickets(new int[] { 25, 25, 50, 100 }));
 
         // => NO : case where the first customer pays with a 50
-        System.out.println(examples.Tickets(new int[] { 50, 50, 50, 50, 50, 50, 50, 50, 50, 50 }));
+        // System.out.println(examples.Tickets(new int[] { 50, 50, 50, 50, 50, 50, 50,
+        // 50, 50, 50 }));
 
         // => YES
-        System.out.println(examples.Tickets(new int[] { 25, 25, 25, 25, 50, 100 }));
+        // System.out.println(examples.Tickets(new int[] { 25, 25, 25, 25, 50, 100 }));
+
+        // => N0
+        // System.out.println(examples.Tickets(new int[] { 25, 25, 25, 25, 25, 25, 25,
+        // 50, 50, 50, 100, 100, 100, 100 }));
+
+        // => YES
+        System.out.println(examples.Tickets(new int[] { 25, 25, 25, 100 }));
     }
 }
