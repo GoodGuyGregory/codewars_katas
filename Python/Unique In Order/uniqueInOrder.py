17c0ef270cd445d11468491c686fbec749902884
GoodGuyGregory <greg.witt625@gmail.com>
2020-12-19 13:32:26 -0500

    adds Unique In Order Solution

--- Python/Unique In Order/uniqueInOrder.py
+++ Python/Unique In Order/uniqueInOrder.py
@@ -2,11 +2,37 @@
 
 
 def unique_in_order(iterable):
-    pass
+    unique = []
+    if type(iterable) != list:
+        iterableList = []
+        #  adds items to a list
+        for element in iterable:
+            iterableList.append(element)
+        value = ""
+        for i in range(len(iterableList)):
+            if value != iterableList[i]:
+                value = iterableList[i]
+                unique.append(iterableList[i])
+    else:
+        value = ""
+        for i in range(len(iterable)):
+            if value != iterable[i]:
+                value = iterable[i]
+                unique.append(iterable[i])
+
+    return print(unique)
 
 
 def main():
-    print()
+    #  TEST CASES:
+    # 'AAAABBBCCDAABBB' => ['A', 'B', 'C', 'D', 'A', 'B']
+    unique_in_order('AAAABBBCCDAABBB')
+
+    # 'ABBCcAD' => ['A', 'B', 'C', 'c', 'A', 'D']
+    unique_in_order('ABBCcAD')
+
+    # [1,2,2,3,3] => [1,2,3]
+    unique_in_order([1, 2, 2, 3, 3])
 
 
 main()
