87750d49ece079967c15a3666708648907ebae4f
GoodGuyGregory <greg.witt625@gmail.com>
2020-12-18 23:10:38 -0500

    adds DNA to RNA conversion Katta

--- Python/DNA RNA Conversion/DNAtoRNAconversion.py
+++ Python/DNA RNA Conversion/DNAtoRNAconversion.py
@@ -1,11 +1,38 @@
 # Kata: https://www.codewars.com/kata/5556282156230d0e5e000089/python
 
 # Solution:
+
+
 def dna_to_rna(dna):
-    return
+    rna = ''
+    for letter in dna:
+        if letter == 'T':
+            rna += 'U'
+        else:
+            rna += letter
+    return rna
+
+#  Favorite Community Solution
+
+
+def dna_RNA_FAV(dna):
+    return dna.replace('T', 'U')
+
 
 def main():
-    
-    
-    
+    #  TEST CASES:
+
+    # "GCAT" => "GCAU"
+    print(dna_to_rna("GCAT"))
+
+    #  "TTTT" => "UUUU"
+    print(dna_to_rna("TTTT"))
+
+    # "GCAT" => "GCAU"
+    print(dna_to_rna("GCAT"))
+
+    # GACCGCCGCC => GACCGCCGCC
+    print(dna_to_rna("GACCGCCGCC"))
+
+
 main()
