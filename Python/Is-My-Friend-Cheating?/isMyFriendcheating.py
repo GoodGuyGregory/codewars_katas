66fa16caf90175a89ab7f108810e7ce0cbcf73d7
GoodGuyGregory <greg.witt625@gmail.com>
2021-01-11 13:36:29 -0500

    adds sumation functionality

--- Python/Is-My-Friend-Cheating?/isMyFriendcheating.py
+++ Python/Is-My-Friend-Cheating?/isMyFriendcheating.py
@@ -2,11 +2,29 @@
 
 
 def remov_nb(n):
+    values = []
     # your code
-    return []
+    #  sum all numbers to n
+    summation = 0
+    i = 1
+    for i in range(1, n+1):
+        summation += i
+        print(summation)
+
+    # determine divisors: 1 is implied to work and not be selected:
+    for i in range(2, n):
+        #  check if the two values work:
+        for j in range(1, n):
+            if i * j == summation - (i * j):
+                # append to list:
+                values.append((i, j))
+                values.append((j, i))
+
+    return values
 
 
 def main():
-    
-    
+    print(remov_nb(26))
+
+
 main()
