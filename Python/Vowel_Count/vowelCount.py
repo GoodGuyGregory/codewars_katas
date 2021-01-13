9b42945eb819c3ba07eb23553549ab5a7202df1f
GoodGuyGregory <greg.witt625@gmail.com>
2021-01-13 08:39:52 -0500

    initial commit for vowel count

---
+++ Python/Vowel_Count/vowelCount.py
@@ -0,0 +1,14 @@
+#  kata: https://www.codewars.com/kata/54ff3102c1bad923760001f3/train/python
+
+
+def get_count(input_str):
+    num_vowels = 0
+    # your code here
+
+    return num_vowels
+
+
+def main():
+
+
+main()
