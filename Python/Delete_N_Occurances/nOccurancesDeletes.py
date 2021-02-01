40bd8db96486b209f3d65e9f9702c946d031100f
GoodGuyGregory <greg.witt625@gmail.com>
2021-02-01 13:29:38 -0500

    initial commit for python kata

--- Python/Delete_N_Occurances/nOccurancesDeletes.py
+++ Python/Delete_N_Occurances/nOccurancesDeletes.py
@@ -1,1 +1,15 @@
 # Kata: https://www.codewars.com/kata/554ca54ffa7d91b236000023
+
+
+def delete_nth(order, max_e):
+
+
+def main():
+    #  provided examples:
+
+    #  delete_nth ([1,1,1,1],2) # return [1,1]
+
+    #   delete_nth ([20,37,20,21],1) # return [20,37,21]
+
+
+main()
