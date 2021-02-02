b5591920ac90a1842e9e9da5b6bc2364cbfc1e2b
GoodGuyGregory <greg.witt625@gmail.com>
2021-02-01 21:23:32 -0500

    adds proposed solution to Delete_N_Occurances

--- Python/Delete_N_Occurances/nOccurancesDeletes.py
+++ Python/Delete_N_Occurances/nOccurancesDeletes.py
@@ -2,14 +2,33 @@
 
 
 def delete_nth(order, max_e):
+    maxDuplicates = max_e
+    removals = []
+    duplicates = 0
+    for i in range(len(order)):
+        element = order[i]
+        remainingElements = order[i+1:len(order)]
+        for j in range(len(remainingElements)):
+            if element == remainingElements[j]:
+                duplicates += 1
+            if duplicates >= max_e:
+                removals.append(1 + j)
+                duplicates = 0
+                break
+    # prepare the list
+    for k in removals:
+        order.pop(k)
+    return order
 
 
 def main():
     #  provided examples:
 
     #  delete_nth ([1,1,1,1],2) # return [1,1]
+    print(delete_nth([1, 1, 1, 1], 2))
 
     #   delete_nth ([20,37,20,21],1) # return [20,37,21]
+    print(delete_nth([20, 37, 20, 21], 1))
 
 
 main()
