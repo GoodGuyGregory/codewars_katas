14e81af148a7d1c99db769d88a242b77c273600d
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-15 14:19:17 -0400

    adds object array for names in prize

--- Javascript/prize_draw/prize_draw.js
+++ Javascript/prize_draw/prize_draw.js
@@ -13,20 +13,47 @@ function rank(st, we, n) {
     }
 
     st = st.toLowerCase();
-    let names = st.split(",")
-    console.log(names);
-    let total = 0;
-    for (let char of st) {
-        if (letterValues.get(char)) {
-            total = total + letterValues.get(char);
+    let names = st.split(",");
+
+    if (!st) {
+        return "No participants";
+    }
+    else if (names.length == we.length && n < names.length) {
+        let nameValues = [];
+
+        let largestValue = 0;
+        for (let i = 0; i < names.length; i++) {
+            let name = names[i];
+            let weight = we[i];
+
+            let total = 0;
+            for (let char of name) {
+                if (letterValues.get(char)) {
+                    total = total + letterValues.get(char);
+                }
+            }
+            total *= weight;
+            // assign name a value in map
+            nameValues.push({
+                "name": name,
+                "total": total,
+            });
         }
+        nameValues.sort((a, b) => b.total - a.total);
+        nameValues.forEach(name => {
+            console.log(name);
+        });
+
 
     }
 
+    else {
+        return "Not enought participants";
+    }
 
 }
 
 // Test Cases:
-
-
-console.log(rank("Lagon, Lily"));
+console.log(rank("", [4, 2, 1, 4, 3, 1, 2], 6));
+console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8));
+console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
