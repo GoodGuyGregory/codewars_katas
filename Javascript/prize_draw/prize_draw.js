2f268d0b29a9c6d32afa8529e7696d6ca8720b10
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-15 19:19:40 -0400

    adds Failed Name Sort Test Case with 78%

--- Javascript/prize_draw/prize_draw.js
+++ Javascript/prize_draw/prize_draw.js
@@ -18,10 +18,9 @@ function rank(st, we, n) {
     if (!st) {
         return "No participants";
     }
-    else if (names.length == we.length && n < names.length) {
+    else if (names.length == we.length && n < names.length + 1) {
         let nameValues = [];
 
-        let largestValue = 0;
         for (let i = 0; i < names.length; i++) {
             let name = names[i];
             let weight = we[i];
@@ -39,11 +38,48 @@ function rank(st, we, n) {
                 "total": total,
             });
         }
-        nameValues.sort((a, b) => b.total - a.total);
-        nameValues.forEach(name => {
-            console.log(name);
+
+        nameValues.sort((a, b) => {
+            if (b.total > a.total) {
+                return 1
+            }
+
+            if (b.total == a.total) {
+                if (b.name.length > a.name.length) {
+                    for (let i = 0; i < b.name.length; i++) {
+                        for (let letter of a.name) {
+                            if (letter > b.name[i]) {
+                                return 1;
+                            }
+                        }
+                    }
+                }
+                else if (b.name.length < a.name.length) {
+                    for (let i = 0; i < a.name.length; i++) {
+                        for (let letter of b.name) {
+                            if (letter > a.name[i]) {
+                                return 1;
+                            }
+                        }
+                    }
+                }
+                else {
+                    for (let i = 0; i < b.name.length; i++) {
+                        for (let letter of a.name) {
+                            if (letter > b.name[i]) {
+                                return 1;
+                            }
+                        }
+                    }
+                }
+
+
+            }
         });
 
+
+        console.log(nameValues);
+        return nameValues[n - 1].name.charAt(0).toUpperCase() + nameValues[n - 1].name.slice(1, nameValues[n - 1].name.length);
 
     }
 
@@ -56,4 +92,11 @@ function rank(st, we, n) {
 // Test Cases:
 console.log(rank("", [4, 2, 1, 4, 3, 1, 2], 6));
 console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8));
+console.log(rank("Lagon,Lily", [1, 5], 2));
 console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
+
+console.log(rank("aubrey,olivai,abigail,chloe,andrew,elizabeth", [3, 1, 4, 4, 3, 2], 4));
+
+console.log(rank("william,willaim,olivia,olivai,lily,lyli", [1, 1, 1, 1, 1, 1], 1));
+
+console.log(rank("Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden", [1, 3, 5, 5, 3, 6], 2));
