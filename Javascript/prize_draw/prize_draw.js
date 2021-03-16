bf5321b74b124f97ab3bff4b76f125cd330c80ba
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-15 23:23:31 -0400

    adds correct prize draw

--- Javascript/prize_draw/prize_draw.js
+++ Javascript/prize_draw/prize_draw.js
@@ -41,24 +41,29 @@ function rank(st, we, n) {
 
         nameValues.sort((a, b) => {
             if (b.total > a.total) {
-                return 1
+                return 1;
+            }
+
+            if (a.total > b.total) {
+                return -1;
             }
 
             if (b.total == a.total) {
                 if (b.name.length > a.name.length) {
                     for (let i = 0; i < b.name.length; i++) {
                         for (let letter of a.name) {
-                            if (letter > b.name[i]) {
+                            if (letter.localeCompare(b.name[i]) >= 1) {
                                 return 1;
                             }
+
                         }
                     }
                 }
                 else if (b.name.length < a.name.length) {
                     for (let i = 0; i < a.name.length; i++) {
                         for (let letter of b.name) {
-                            if (letter > a.name[i]) {
-                                return 1;
+                            if (letter.localeCompare(a.name[i]) >= 1) {
+                                return -1;
                             }
                         }
                     }
@@ -66,7 +71,7 @@ function rank(st, we, n) {
                 else {
                     for (let i = 0; i < b.name.length; i++) {
                         for (let letter of a.name) {
-                            if (letter > b.name[i]) {
+                            if (letter.localeCompare(b.name[i]) == 1) {
                                 return 1;
                             }
                         }
@@ -77,26 +82,26 @@ function rank(st, we, n) {
             }
         });
 
-
-        console.log(nameValues);
         return nameValues[n - 1].name.charAt(0).toUpperCase() + nameValues[n - 1].name.slice(1, nameValues[n - 1].name.length);
 
     }
 
     else {
-        return "Not enought participants";
+        return "Not enough participants";
     }
 
 }
 
 // Test Cases:
 console.log(rank("", [4, 2, 1, 4, 3, 1, 2], 6));
-console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8));
-console.log(rank("Lagon,Lily", [1, 5], 2));
-console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
+// console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 8));
+// console.log(rank("Lagon,Lily", [1, 5], 2));
+// console.log(rank("Addison,Jayden,Sofia,Michael,Andrew,Lily,Benjamin", [4, 2, 1, 4, 3, 1, 2], 4));
 
-console.log(rank("aubrey,olivai,abigail,chloe,andrew,elizabeth", [3, 1, 4, 4, 3, 2], 4));
+// console.log(rank("aubrey,olivai,abigail,chloe,andrew,elizabeth", [3, 1, 4, 4, 3, 2], 4));
 
 console.log(rank("william,willaim,olivia,olivai,lily,lyli", [1, 1, 1, 1, 1, 1], 1));
 
 console.log(rank("Elijah,Chloe,Elizabeth,Matthew,Natalie,Jayden", [1, 3, 5, 5, 3, 6], 2));
+
+console.log(rank("Mason,Sophia,Lagon,Lyli,Jayden,Joshua,Daniel,Ella,Mia,Naoh,Aiden,Jacob,Elizabeth,James", [1, 4, 3, 3, 1, 1, 1, 1, 2, 5, 5, 3, 1, 2], 7));
