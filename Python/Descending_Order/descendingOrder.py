d447cb38b05fbaa3738508a39df0b3bc8869dc49
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-23 22:51:49 -0400

    adds vscode modification for python and desc kata

--- Python/Descending_Order/descendingOrder.py
+++ Python/Descending_Order/descendingOrder.py
@@ -3,11 +3,17 @@
 
 def descending_order(num):
     # Bust a move right here
-
-    # Test Cases:
+    while num % 10 != 0:
+        value = num % 10
+        num //= 10
+        largest = 0
+        if value > largest:
+            largest = value
 
 
 def main():
+    # Test Cases:
+    descending_order(42145)
 
 
 main()
