7f1041fb786efdba45bf86ccdcd874d6eec3f6c1
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-25 10:58:30 -0400

    adds number of carries

---
+++ Python/Number-Of-Carries/numberOfCarries.py
@@ -0,0 +1,20 @@
+# Kata: https://www.codewars.com/kata/58a6568827f9546931000027/train/python
+
+
+def number_of_carries(a, b):
+    # coding and coding..
+
+
+def main():
+    # Test Cases:
+    # returns 0
+    number_of_carries(543, 3456)
+    # returns 2
+    number_of_carries(1927, 6426)
+    #  returns 4
+    number_of_carries(9999, 1)
+    # returns 2
+    number_of_carries(1234, 5678)
+
+
+main()
