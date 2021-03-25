16e0711e5e7b7002252bffd5fd09028cc79da2ad
GoodGuyGregory <greg.witt625@gmail.com>
2021-03-25 12:57:11 -0400

    adds 3 of 4 passing tests to kata

--- Python/Number-Of-Carries/numberOfCarries.py
+++ Python/Number-Of-Carries/numberOfCarries.py
@@ -3,17 +3,33 @@
 
 def number_of_carries(a, b):
     # coding and coding..
+    carries = 0
+    if (len(str(a)) > len(str(b))):
+        longest = str(a)
+        shorter = str(b)[::-1]
+    else:
+        longest = str(b)
+        shorter = str(a)[::-1]
+    k = 0
+    for n in reversed(longest):
+        while k < len(shorter):
+            print("n: %s k: %s" % (n, shorter[k]))
+            if int(n) + int(shorter[k]) > 9:
+                carries += 1
+            k += 1
+            break
+    print("carries: %s" % carries)
 
 
 def main():
     # Test Cases:
     # returns 0
     number_of_carries(543, 3456)
-    # returns 2
+    # # returns 2
     number_of_carries(1927, 6426)
-    #  returns 4
+    # #  returns 4
     number_of_carries(9999, 1)
-    # returns 2
+    # # returns 2
     number_of_carries(1234, 5678)
 
 
