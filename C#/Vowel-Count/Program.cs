4bddf401374fb5d9cbca0080459241b9aa7c6a04
GoodGuyGregory <greg.witt625@gmail.com>
2021-05-13 13:58:20 -0400

    adds working kata solution in C#

--- C#/Vowel-Count/Program.cs
+++ C#/Vowel-Count/Program.cs
@@ -7,16 +7,27 @@ namespace Vowel_Count
 
         public static int GetVowelCount(string str)
         {
-            int vowelCount = 0;
-
             
+            char[] vowels = {'a','e','i','o','u'};
+            int vowelCount = 0;
+            // Console.WriteLine(str);
+
+            foreach(char letter in str) {
+                // Console.WriteLine(letter);
+                if (Array.IndexOf(vowels, letter) > -1) {
+                    vowelCount++;
+                } 
+            }
+
+            Console.WriteLine(vowelCount);
 
             return vowelCount;
         }
 
         static void Main(string[] args)
         {
-            
+            // Should Return 5 "abracadabra"
+            GetVowelCount("abracadabra");
         }
     }
 }
