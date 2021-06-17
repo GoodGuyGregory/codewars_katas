efdc2d69780ec3b62c540d54d1a527987bca5ded
GoodGuyGregory <greg.witt625@gmail.com>
2021-06-17 12:00:28 -0400

    adds initial commit for Tank_Truck

---
+++ Java/Tank_Truck/Truck_Tank.java
@@ -0,0 +1,15 @@
+// Kata: https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/java
+
+public class Truck_Tank {
+
+    // Kata Method for Calculating Remaining Liquid:
+    public class VolTank {
+        public static int tankVol(int h, int d, int vt) {
+            // your code
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
