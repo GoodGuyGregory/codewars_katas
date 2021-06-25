4cefa0903450f27d7870cdb362292577d4a1f84d
GoodGuyGregory <greg.witt625@gmail.com>
2021-06-25 14:12:26 -0400

    adds examples for test data on kata

--- Javascript/training_on_a_tank_truck/tankTruck.js
+++ Javascript/training_on_a_tank_truck/tankTruck.js
@@ -1,1 +1,18 @@
 // kata: https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/java
+
+public class VolTank {
+    public static int tankVol(int h, int d, int vt) {
+        // your code
+    }
+}
+
+
+//  Test Data:
+// should return 1021 (calculation gives about: 1021.26992027)
+tankvol(40, 120, 3500);
+
+// should return 1750
+tankvol(60, 120, 3500);
+
+// should return 2478 (calculation gives about: 2478.73007973)
+tankvol(80, 120, 3500);
