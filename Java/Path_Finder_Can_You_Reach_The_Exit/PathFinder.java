ca4849447770392ff1a2d35aa80d078ab0b55cfe
GoodGuyGregory <greg.witt625@gmail.com>
2021-08-17 10:52:51 -0400

    initial commit for Path Finder

---
+++ Java/Path_Finder_Can_You_Reach_The_Exit/PathFinder.java
@@ -0,0 +1,77 @@
+import java.util.regex.Pattern;
+
+//  Kata: https://www.codewars.com/kata/5765870e190b1472ec0022a2/train/java
+
+public class PathFinder {
+    // static boolean pathFinder(String maze) {
+    // // Your code here!!
+    // return false;
+    // }
+
+    public static void pathFinder(String maze) {
+        // check if the initial path is blocked
+        if (maze.indexOf(0) == 'W') {
+            // return false;
+            System.out.println("Can't Complete");
+            // return false;
+        }
+
+        // create a mazeArray based on incoming string
+        char[][] mazeProblem = new char[maze.indexOf('\n')][maze.indexOf('\n')];
+
+        // fix '\n' issue
+        // maze.split(Pattern.matches([\n], maze));
+        String[] splitA = maze.split("\\n");
+        String fullMaze = "";
+        for (String mazeRows : splitA) {
+            fullMaze += mazeRows;
+        }
+
+        // check concat logic:
+        // System.out.println(fullMaze);
+
+        // enqueue the string values:
+        int k = 0;
+        while (k < fullMaze.length()) {
+            for (int i = 0; i < mazeProblem.length; i++) {
+                for (int j = 0; j < mazeProblem.length; j++) {
+                    // add maze entry
+                    mazeProblem[i][j] += fullMaze.charAt(k);
+                    k++;
+                }
+            }
+        }
+
+        // print working letters:
+        // for (int i = 0; i < mazeProblem.length; i++) {
+        // for (int j = 0; j < mazeProblem.length; j++) {
+        // System.out.print(mazeProblem[i][j]);
+        // }
+        // }
+    }
+
+    public static void main(String[] args) {
+
+        PathFinder testPathFinder = new PathFinder();
+
+        String a = ".W.\n" + ".W.\n" + "...",
+
+                b = ".W.\n" + ".W.\n" + "W..",
+
+                c = "......\n" + "......\n" + "......\n" + "......\n" + "......\n" + "......",
+
+                d = "......\n" + "......\n" + "......\n" + "......\n" + ".....W\n" + "....W.";
+
+        // System.out.println(a);
+        // System.out.println(a.split(Pattern.matches("[\n]", a)));
+        // System.out.println(a.indexOf('\n'));
+
+        // TESTS:
+        // pathFinder(a) -> true
+        testPathFinder.pathFinder(a);
+        // pathFinder(b) -> false
+        // pathFinder(c) -> true
+        // pathFinder(d) -> false
+    }
+
+}
---
+++ Java/Path_Finder_Can_You_Reach_The_Exit/notes.md
@@ -0,0 +1,2 @@
+[Create a 2D Array](https://www.geeksforgeeks.org/different-ways-to-declare-and-initialize-2-d-array-in-java/)
+[indexOf Documentation](https://www.javatpoint.com/java-string-indexof)
