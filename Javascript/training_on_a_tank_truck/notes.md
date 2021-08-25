267305d3f4f003e25ea7ba3f14bc268fecf89561
GoodGuyGregory <greg.witt625@gmail.com>
2021-08-25 16:05:14 -0400

    adds notes to Truck Kata and initial commit for testing

--- Javascript/training_on_a_tank_truck/notes.md
+++ Javascript/training_on_a_tank_truck/notes.md
@@ -3,3 +3,23 @@
 [Dipstick Logic](https://math.stackexchange.com/questions/972129/volume-of-a-horizontal-cylinder-using-height-of-liquid)
 
 [Calculating Volume based on Circle Segment Method Wiki](https://en.wikipedia.org/wiki/Circular_segment)
+
+[Volume of a Segment Area](https://www.mathopenref.com/cylindervolpartial.html)
+
+[Equation for a Segment Area](https://www.mathopenref.com/segmentareaht.html)
+
+## Correct Formulas:
+
+**Segement Area**
+
+![Sement Area Volume](./img/AreaSegmentVol.JPG)
+
+**Sement Area Deduction Process**
+
+```
+ Volume = pi*r^2*height
+ height = Volume/(pi*r^2)
+```
+**Volume of a Cylinder**
+
+use the volume of the cylinder equation to deduct the height of the cylinder
--- Javascript/training_on_a_tank_truck/tankTruck.js
+++ Javascript/training_on_a_tank_truck/tankTruck.js
@@ -1,15 +1,20 @@
 // kata: https://www.codewars.com/kata/55f3da49e83ca1ddae0000ad/train/java
 
-public class VolTank {
-    public static int tankVol(int h, int d, int vt) {
-        // your code
-    }
+function tankvol(h, d, vt) {
+    let cylinderRadius = d / 2;
+    let heightOfCylinder = vt / (Math.PI * (cylinderRadius * cylinderRadius));
+    // determine area of segment with liquid
+    let areaVolume = (cylinderRadius * cylinderRadius) * Math.acos((cylinderRadius - heightOfCylinder) / cylinderRadius) - (cylinderRadius - heightOfCylinder) * Math.sqrt(2 * (cylinderRadius * heightOfCylinder) - (heightOfCylinder * heightOfCylinder));
+
+    let segmentVolume = heightOfCylinder * areaVolume;
+    return Math.floor(segmentVolume);
 }
+
 
 
 //  Test Data:
 // should return 1021 (calculation gives about: 1021.26992027)
-tankvol(40, 120, 3500);
+console.log(tankvol(40, 120, 3500));
 
 // should return 1750
 tankvol(60, 120, 3500);
