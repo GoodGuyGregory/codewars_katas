87937f5d474a6d0d286459e8c32c3a268d2edc0a
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-11 14:33:23 -0400

    adds twice as old kata

--- Javascript/twice_as_old_as/twiceAsOldAs.js
+++ Javascript/twice_as_old_as/twiceAsOldAs.js
@@ -1,11 +1,12 @@
 // Kata: https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
 
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-
+    let twiceSonAge = dadYearsOld - (sonYearsOld * 2);
+    return Math.abs(twiceSonAge)
 }
 
 //  Test Data:
 // "Testing for dad's age: 36 and son's age: 7": Should Yield 22
-twiceAsOld(36, 7)
+console.log(twiceAsOld(36, 7))
 // "Testing for dad's age: 55 and son's age: 30": Should Yield 5
-twiceAsOld(55, 30)
+console.log(twiceAsOld(55, 30))
