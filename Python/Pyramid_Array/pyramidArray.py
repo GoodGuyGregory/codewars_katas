1c1458e042b948a88d7909d7839fc372d4293f8b
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-17 15:20:21 -0400

    initial commit for kata

---
+++ Python/Pyramid_Array/pyramidArray.py
@@ -0,0 +1,21 @@
+#  Kata: https://www.codewars.com/kata/515f51d438015969f7000013/train/python
+
+
+def pyramid(n):
+    #
+
+
+def main():
+
+    #  test values:
+    #  should respond with []
+    pyramid(0)
+    #  should respond with [[1]]
+    pyramid(1)
+    # should respond with a list of arrays as [[1],[1,1]]
+    pyramid(2), [[1], [1, 1]]
+    # should respond with three nested arrays [[1], [1, 1], [1, 1, 1]]
+    pyramid(3)
+
+
+main()
