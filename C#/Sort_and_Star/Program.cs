73d1a345de32e628a053dca9a6f12cf0e6935ffc
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-23 09:56:08 -0400

    initial commit for Sort_and_Star

---
+++ C#/Sort_and_Star/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sort_and_Star
+{
+    class Program
+    {
+        // Kata: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/csharp
+        public static string TwoSort(string[] s)
+        {
+            return "";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
