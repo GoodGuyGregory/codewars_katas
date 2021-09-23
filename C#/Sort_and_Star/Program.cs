28ec35b684245945a3a3cd58402cab96ccaa8aff
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-23 10:32:40 -0400

    adds initial test cases to repo

--- C#/Sort_and_Star/Program.cs
+++ C#/Sort_and_Star/Program.cs
@@ -5,6 +5,8 @@ namespace Sort_and_Star
     class Program
     {
         // Kata: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/csharp
+
+        // You should not remove or add elements from/to the array.
         public static string TwoSort(string[] s)
         {
             return "";
@@ -12,7 +14,11 @@ namespace Sort_and_Star
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // test cases:
+            // passing returns "b***i***t***c***o***i***n"
+            TwoSort(new[] { "bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps" });
+            //  passing returns  "a***r***e"
+            TwoSort(new[] { "turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones" });
         }
     }
 }
