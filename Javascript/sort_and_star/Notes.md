9605ccc8352adca1e1a5934b3fc47699fc87a7d0
GoodGuyGregory <greg.witt625@gmail.com>
2021-09-24 19:01:02 -0400

    adds notes and initial logic for kata

---
+++ Javascript/sort_and_star/Notes.md
@@ -0,0 +1,5 @@
+## Sort and Star
+-----
+
+* [W3 CharCodeAt Method](https://www.w3schools.com/jsref/jsref_charcodeat.asp)
+* [MDN Sort](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort)
--- Javascript/sort_and_star/sort_and_star.js
+++ Javascript/sort_and_star/sort_and_star.js
@@ -1,6 +1,10 @@
 // Kata: https://www.codewars.com/kata/57cfdf34902f6ba3d300001e/train/csharp
 function twoSort(s) {
-
+    let sortableArray = s;
+    sortableArray.sort(function compareElements(element1, element2) {
+        return element1.charCodeAt(0) - element2.charCodeAt(0);
+    });
+    console.log(sortableArray);
 }
 
 // Test Cases:
