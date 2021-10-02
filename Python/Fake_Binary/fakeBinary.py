4d1e3e6feb73d1428a63a8776e9e9ce2f5609ec4
GoodGuyGregory <greg.witt625@gmail.com>
2021-10-02 11:22:33 -0400

    adds fake-binary in python

--- Python/Fake_Binary/fakeBinary.py
+++ Python/Fake_Binary/fakeBinary.py
@@ -2,10 +2,28 @@
 
 
 def fake_bin(x):
-    pass
+    fake_binary = ""
+    for i in range(len(x)):
+        if int(x[i]) < 5:
+            fake_binary += "0"
+        else:
+            fake_binary += "1"
+    return fake_binary
 
 
 def main():
+
+    # test cases:
+    #  "01011110001100111"
+    print(fake_bin("45385593107843568"))
+    #   "101000111101101"
+    print(fake_bin("509321967506747"))
+    #  "011011110000101010000011011"
+    print(fake_bin("366058562030849490134388085"))
+    # "01111100",
+    print(fake_bin("15889923"))
+    # "100111001111"
+    print(fake_bin("800857237867"))
 
 
 main()
