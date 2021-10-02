f4875afb080abdbc05774ca21a419ed013d73899
GoodGuyGregory <greg.witt625@gmail.com>
2021-10-02 11:06:45 -0400

    adds fake binary and cleans solution files

--- C#/clean.sh
+++ C#/clean.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
-
-<<<<<<< HEAD
 read -p "What's the Kata you're working on? " KATA
 echo "Cleaning your project for remote push..."
 cd ./$KATA
 pwd
 rm -vr obj
+rm -vr bin
 rm -vr $KATA.csproj
 
