792066da551ea4208b932e40d338341d710f5d42
GoodGuyGregory <greg.witt625@gmail.com>
2021-10-02 00:21:38 -0400

    initial commit for kata

---
+++ C#/Fake-Binary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fake_Binary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
