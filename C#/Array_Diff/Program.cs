5e58458cc40e9aaea1c125264cebdf261648d90b
GoodGuyGregory <greg.witt625@gmail.com>
2021-10-11 10:12:06 -0400

    initial commit for Array_Diff

---
+++ C#/Array_Diff/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Array_Diff
+{
+    class Program
+    {
+        // Kata: https://www.codewars.com/kata/523f5d21c841566fde000009/train/csharp
+
+          public static int[] ArrayDiff(int[] a, int[] b)
+        {
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Test Cases:
+
+            // Returns [2] 
+            ArrayDiff(new int[] {1, 2},    new int[] {1});
+            
+            // Returns [2,2] 
+            ArrayDiff(new int[] {1, 2, 2}, new int[] {1});
+            
+            // Returns [1]   
+            ArrayDiff(new int[] {1, 2, 2}, new int[] {2});
+            
+            // Returns [1,2,3]   
+            ArrayDiff(new int[] {1, 2, 2}, new int[] {});
+            
+            // Return []   
+            ArrayDiff(new int[] {},        new int[] {1, 2});
+            
+            // Return [3]   
+            ArrayDiff(new int[] {1, 2, 3}, new int[] {1, 2});
+        }
+            
+        
+    
+}
