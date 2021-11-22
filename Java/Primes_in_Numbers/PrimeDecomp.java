c96ed52821fe01e2291ffae495e4eab9a5f19ddb
GoodGuyGregory <greg.witt625@gmail.com>
2021-11-21 22:23:25 -0500

    adds working implementation

--- Java/Primes_in_Numbers/PrimeDecomp.java
+++ Java/Primes_in_Numbers/PrimeDecomp.java
@@ -1,6 +1,8 @@
+
 // Kata: https://www.codewars.com/kata/54d512e62a5e54c96200019e/train/java
 
 import java.util.*;
+import java.util.TreeMap;
 
 public class PrimeDecomp {
 
@@ -8,7 +10,15 @@ public class PrimeDecomp {
         int[] primeFactors = { 2, 3, 5, 7, 11, 13, 17 };
         // TODO:
         // Create Results Hashmap for each prime values:
-        // due to the fact that there could be multiples:
+        // due to the fact that there could be multiples
+        HashMap<Integer, Integer> primefactorValues = new HashMap<>();
+
+        for (int number : primeFactors) {
+            // casts element to string for enqueue
+            primefactorValues.put(number, 0);
+        }
+
+        Map<Integer, Integer> sortedFactorMap = new TreeMap<Integer, Integer>(primefactorValues);
 
         // holds factors based on divisibility
         ArrayList<Integer> resultingFactors = new ArrayList<Integer>(0);
@@ -22,10 +32,31 @@ public class PrimeDecomp {
             }
         }
 
-        for (Integer result : resultingFactors) {
-            System.out.println(result);
+        for (int factorKey : sortedFactorMap.keySet()) {
+            int totalElementFactorsFound = 0;
+            for (Integer element : resultingFactors) {
+                // found element that needs to be added to Hashmap
+                if (element == factorKey) {
+                    // increase the found count
+                    totalElementFactorsFound += 1;
+                }
+            }
+            sortedFactorMap.replace(factorKey, totalElementFactorsFound);
         }
-        return "Finished";
+
+        // Build Resulting Decomposed string
+        StringBuilder decompFactors = new StringBuilder();
+        for (int factorKey : sortedFactorMap.keySet()) {
+            if (sortedFactorMap.get(factorKey) == 0) {
+                continue;
+            } else if (sortedFactorMap.get(factorKey) > 1) {
+                String numOfFactors = String.valueOf(sortedFactorMap.get(factorKey));
+                decompFactors.append("(" + factorKey + "**" + numOfFactors + ")");
+            } else {
+                decompFactors.append("(" + String.valueOf(factorKey) + ")");
+            }
+        }
+        return decompFactors.toString();
     }
 
     public static void main(String[] arg) {
@@ -35,7 +66,7 @@ public class PrimeDecomp {
         System.out.println(factors(30));
 
         // 86240 :: should return "(2**5)(5)(7**2)(11)"
-        // System.out.println(factors(86240));
+        System.out.println(factors(86240));
 
     }
 }
