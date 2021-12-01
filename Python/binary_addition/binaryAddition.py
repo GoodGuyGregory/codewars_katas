6ba831a74c5eadfc0cd881d0a9bd038ebf204a90
GoodGuyGregory <greg.witt625@gmail.com>
2021-12-01 12:46:05 -0500

    adds test case data for kata

--- Python/binary_addition/binaryAddition.py
+++ Python/binary_addition/binaryAddition.py
@@ -1,1 +1,22 @@
 # Kata https://www.codewars.com/kata/551f37452ff852b7bd000139/train/python
+
+
+def add_binary(a, b):
+    return a + b
+
+
+def main():
+    # Test Cases:
+    # "10"
+    print(add_binary(1, 1))
+    # "1"
+    print(add_binary(0, 1))
+    # "1"
+    print(add_binary(1, 0))
+    # "100"
+    print(add_binary(2, 2))
+    # "111111"
+    print(add_binary(51, 12))
+
+
+main()
