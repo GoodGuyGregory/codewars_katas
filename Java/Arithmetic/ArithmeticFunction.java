d7e3a22140e3c8eae0193dd61648612d230f3268
GoodGuyGregory <greg.witt625@gmail.com>
2021-12-02 13:14:33 -0500

    iniital commit for Arithmetic Kata Function

---
+++ Java/Arithmetic/ArithmeticFunction.java
@@ -0,0 +1,7 @@
+// Kata: https://www.codewars.com/kata/583f158ea20cfcbeb400000a/train/java
+
+class ArithmeticFunction {
+  public static int arithmetic(int a, int b, String operator) {
+    // your code here
+  }
+}
