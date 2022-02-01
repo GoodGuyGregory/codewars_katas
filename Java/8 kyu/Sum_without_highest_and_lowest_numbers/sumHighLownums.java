d36d2d51e6ec639f579e00d985324d827d7170b3
GoodGuyGregory <greg.witt625@gmail.com>
2022-02-01 10:23:12 -0500

    Sum High Low numbers kata's initial commit

---
+++ Java/8 kyu/Sum_without_highest_and_lowest_numbers/sumHighLownums.java
@@ -0,0 +1,20 @@
+// Kata: https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/java
+
+public class SumHighLownums {
+    public static int sum(int[] numbers)
+    {
+      return 0;
+    }
+
+    public static void main(String[] args) {
+        SumHighLownums testCases = new SumHighLownums();
+
+        // TEST CASES:
+        // =======================================
+
+        // { 6, 2, 1, 8, 10 } => 16
+        // { 1, 1, 11, 2, 3 } => 6
+
+        
+    }
+}
