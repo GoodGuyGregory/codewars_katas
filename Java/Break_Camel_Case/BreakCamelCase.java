d9e03fb686795c7773f12ee218cd276256b6fb9e
GoodGuyGregory <greg.witt625@gmail.com>
2022-01-31 19:24:47 -0500

    adds camelCaseBreak Kata

--- Java/Break_Camel_Case/BreakCamelCase.java
+++ Java/Break_Camel_Case/BreakCamelCase.java
@@ -1,5 +1,48 @@
 // Kata: https://www.codewars.com/kata/5208f99aee097e6552000148/train/java
+import java.lang.*;
 
 public class BreakCamelCase {
-    
+    public static String camelCase(String input) {
+        StringBuilder camelCaseBreak = new StringBuilder();
+        if (input.isEmpty()) {
+            return input;
+        }
+        else {
+            
+            char[] inputLetters = input.toCharArray();
+            for (int i = 0; i < inputLetters.length; i++) {
+                if (Character.isUpperCase(inputLetters[i])) {
+                    // String foundLetter = String.format("%s is uppercase, found at %s",inputLetters[i], i);
+                    // System.out.println(foundLetter);
+                    camelCaseBreak.append(" " + inputLetters[i]);
+                }
+                else {
+                    camelCaseBreak.append(inputLetters[i]);
+                }
+                
+            }
+        }
+        return camelCaseBreak.toString();
+      }
+      
+      public static void main(String[] args) {
+          
+        BreakCamelCase testCases = new BreakCamelCase();
+        
+        // TEST CASES:
+        // =================================================
+        
+        // "camelCasing"  =>  "camel Casing"
+        // "identifier"   =>  "identifier"
+        // ""             =>  ""
+        
+        System.out.println(camelCase("inputString"));
+        System.out.println(camelCase("camelCasing"));
+        System.out.println(camelCase("identifier"));
+        System.out.println(camelCase(""));
+        System.out.println(camelCase("camelCasingTest"));
+        System.out.println(camelCase("camelcasingtest"));
+    }
+
+
 }
