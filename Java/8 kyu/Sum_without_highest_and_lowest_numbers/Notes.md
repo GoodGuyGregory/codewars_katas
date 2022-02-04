64e503d4942845682a4a07c1bede86e9717951f9
Greg Witt <greg.witt@avnet.com>
2022-02-04 08:57:06 -0500

    Removes elements and iterate methods and notes for implementation strategy

--- Java/8 kyu/Sum_without_highest_and_lowest_numbers/Notes.md
+++ Java/8 kyu/Sum_without_highest_and_lowest_numbers/Notes.md
@@ -1,7 +1,9 @@
+## Implementation Notes:
+
+**Parsing the Array for Max or Min**
 
 instead of making multiple comparisons, just compare all of the values with one single value
 from the given array and make a decision of highest or lowest based on that comparison value only.
-
 
 
 ```java
@@ -41,4 +43,23 @@ class MinMaxExample {
     return minValue; 
   } 
 }
+```
+
+**Creating an Arraylist from int[]**
+
+There is no shortcut for converting from int[] to List<Integer> as Arrays.asList does not deal with boxing and will just create a List<int[]> which is not what you want. You have to make a utility method.
+
+```java
+int[] ints = {1, 2, 3};
+List<Integer> intList = new ArrayList<Integer>(ints.length);
+for (int i : ints)
+{
+    intList.add(i);
+}
+
+// remove the element at the index found, to ensure the remaining values remain inside of the list
+
+  // This makes a call to remove(Object) and
+        // removes element 1
+        al.remove(intList.indexOf(highest || or Lowest));
 ```
--- Java/8 kyu/Sum_without_highest_and_lowest_numbers/sumHighLownums.java
+++ Java/8 kyu/Sum_without_highest_and_lowest_numbers/sumHighLownums.java
@@ -1,9 +1,15 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Kata: https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/java
 
 public class SumHighLownums {
     public static int sum(int[] numbers){
         int highest = 0;
         int lowest = 0;
+        
         for (int i : numbers) {
             // find highest
             if (i >= numbers[0] && i > highest) {
@@ -15,9 +21,24 @@ public class SumHighLownums {
             }
         }
 
+        // create arraylist to hold and remove the first index 
+        // of the higest and lowest values in the list of nums
+
+        List<Integer> numsList = new ArrayList<Integer>(numbers.length);
+
+        for (int i : numbers) {
+            numsList.add(i);
+        }
+
+        // removes highest
+        numsList.remove(numsList.indexOf(highest));
+        // removes lowest
+        numsList.remove(numsList.indexOf(lowest));
+
+
         int sum = 0;
         
-        for (int i : numbers) {
+        for (int i : numsList) {
                 sum += i;
             }
         
