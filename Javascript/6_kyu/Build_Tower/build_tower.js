d0a6912a04f056f7b28b35778ec84175441eaaa4
GoodGuyGregory <greg.witt625@gmail.com>
2022-02-22 13:56:59 -0500

    Initial commit for towerBuilder

---
+++ Javascript/6_kyu/Build_Tower/build_tower.js
@@ -0,0 +1,28 @@
+function towerBuilder(nFloors) {
+    let i = 1;
+    let tower = []
+    for ()
+}
+
+
+// Test Cases:
+
+// towerBuilder(3) => [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+
+    console.log(towerBuilder(3));
+
+
+// towerBuilder(6) => [
+    //   "     *     ", 
+    //   "    ***    ", 
+    //   "   *****   ", 
+    //   "  *******  ", 
+    //   " ********* ", 
+    //   "***********"
+    // ]
+
+    console.log(towerBuilder(3));
