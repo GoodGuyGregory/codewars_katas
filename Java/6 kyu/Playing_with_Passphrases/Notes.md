7d65ec3a825277950462704d1e5ae2dae8797684
Greg Witt <greg.witt@avnet.com>
2022-04-04 11:09:02 -0400

    Adds notes for initial first step to passphrase method

---
+++ Java/6 kyu/Playing_with_Passphrases/Notes.md
@@ -0,0 +1,5 @@
+## Playing with Passphrases Notes:
+
+1. [Determine ASCII](https://www.programiz.com/java-programming/examples/ascii-value-character) ranges and values for numbers, based on Char conversions to determine lower or upper case complements to the supplied numbers for the method. 
+
+**[ASCII Chart](https://www.javatpoint.com/java-ascii-table) for reference**
--- Java/6 kyu/Playing_with_Passphrases/Playing_Passphrases.java
+++ Java/6 kyu/Playing_with_Passphrases/Playing_Passphrases.java
@@ -1,1 +1,23 @@
 // Kata: https://www.codewars.com/kata/559536379512a64472000053/train/java
+
+public class Playing_Passphrases {
+    
+    //  Steps in this process...
+    1.    shift each letter by a given number but the transformed letter must be a letter (circular shift),
+    2. replace each digit by its complement to 9,
+    3. keep such as non alphabetic and non digit characters,
+    4. downcase each letter in odd position, upcase each letter in even position (the first character is in position 0),
+    5. reverse the whole result.
+
+    public static String playPass(String s, int n) {
+
+    }
+
+    public static void main(String[] args) {
+        
+        playPass( "BORN IN 2015!", 1);
+
+
+    }
+
+}
